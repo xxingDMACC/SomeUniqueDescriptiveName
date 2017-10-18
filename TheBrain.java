43daf9d594deb6bc91e1b5fe55e940a35a7fdc3b
xxingDMACC <xxing@dmacc.edu>
2017-10-18 01:51:16 -0500

    Finally test this.

--- TheBrain.java
+++ TheBrain.java
@@ -5,6 +5,7 @@ public class TheBrain {
 	public static void main(String[] args) {
 		System.out.println("does it work?");
 		System.out.println("So far, so good...");
+		System.out.println("Hello Xiaoyu");
 		// add more
 	}
 }
